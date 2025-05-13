0afff97592a48bdb23fa9018f1a37cbd8fd29d12
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-13 19:47:30 -0300

    feat: add OrderItem, OrderItemPK and OrderItemRepository class

--- course/course/src/main/java/com/gabrielssmacedo/course/config/TestConfig.java
+++ course/course/src/main/java/com/gabrielssmacedo/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.gabrielssmacedo.course.entities.Category;
 import com.gabrielssmacedo.course.entities.Order;
+import com.gabrielssmacedo.course.entities.OrderItem;
 import com.gabrielssmacedo.course.entities.Product;
 import com.gabrielssmacedo.course.entities.User;
 import com.gabrielssmacedo.course.entities.enums.OrderStatus;
 import com.gabrielssmacedo.course.repositories.CategoryRepository;
+import com.gabrielssmacedo.course.repositories.OrderItemRepository;
 import com.gabrielssmacedo.course.repositories.OrderRepository;
 import com.gabrielssmacedo.course.repositories.ProductRepository;
 import com.gabrielssmacedo.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -70,5 +75,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
