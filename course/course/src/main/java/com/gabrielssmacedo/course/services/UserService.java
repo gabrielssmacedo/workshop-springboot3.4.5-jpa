747a22d40385a8bd0165abb23d4ecf605bb0857d
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-15 15:08:33 -0300

    feat: implements personalized exception handling

--- course/course/src/main/java/com/gabrielssmacedo/course/services/UserService.java
+++ course/course/src/main/java/com/gabrielssmacedo/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gabrielssmacedo.course.entities.User;
 import com.gabrielssmacedo.course.repositories.UserRepository;
+import com.gabrielssmacedo.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
